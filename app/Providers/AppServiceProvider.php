f2a06f59266b5be883253d0846cd7d9009610752
raihan-js <araihansikder@gmail.com>
2024-11-14 00:59:13 +0600

    https issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ($this->app->environment('production')) {
+            // Force HTTPS routes
+            URL::forceScheme('https');
+        }
+        
         Blade::directive('markdown', function ($expression) {
             return "<?php echo (new Parsedown)->text($expression); ?>";
         });
